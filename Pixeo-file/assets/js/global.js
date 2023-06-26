784d4e38493699b39289571f2a7bf47a4779175f
Parimal Priyanshu <parimalpriyanshu@gmail.com>
2023-06-26 10:05:35 +0530

    newer one

--- Pixeo-file/assets/js/global.js
+++ Pixeo-file/assets/js/global.js
@@ -12,6 +12,6 @@ const addEventOnElements = function(elements,eventType,callback){
 const searchBox = document.querySelector("[search-box]");
 const searchTogglers = document.querySelectorAll("[search-toggler]");
 
-addEventOnElements(searchTogglers, "click",function (){
+addEventOnElements(searchTogglers, "click",function () {
     searchBox.classList.toggle("active");
-} );
+});
